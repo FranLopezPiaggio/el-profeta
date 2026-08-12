70aa363751354d494326bd0347d31578e267ff81
FranLP <francolopezpiaggio@gmail.com>
2026-08-11 21:54:40 -0300

    delete old functions

--- src/domain/use-cases/create-product.ts
+++ src/domain/use-cases/create-product.ts
@@ -1,22 +1,22 @@
-import { Product, CreateProductInput } from "../entities/product.entity";
+// import { Product, CreateProductInput } from "../entities/product.entity";
 
-export class CreateProductUseCase {
-    // Las reglas de negocio van aquí (ej. validaciones de negocio)
-    execute(input: CreateProductInput): Product {
-        if (input.price <= 0) {
-            throw new Error("El precio debe ser mayor a 0");
-        }
+// export class CreateProductUseCase {
+//     // Las reglas de negocio van aquí (ej. validaciones de negocio)
+//     execute(input: CreateProductInput): Product {
+//         if (input.price <= 0) {
+//             throw new Error("El precio debe ser mayor a 0");
+//         }
 
-        if (input.stock < 0) {
-            throw new Error("El stock no puede ser negativo");
-        }
+//         if (input.stock < 0) {
+//             throw new Error("El stock no puede ser negativo");
+//         }
 
-        const newProduct: Product = {
-            id: crypto.randomUUID(), // O generado por persistencia
-            ...input,
-            createdAt: new Date(),
-        };
+//         const newProduct: Product = {
+//             id: crypto.randomUUID(), // O generado por persistencia
+//             ...input,
+//             createdAt: new Date(),
+//         };
 
-        return newProduct;
-    }
-}
+//         return newProduct;
+//     }
+// }
--- src/ui/actions/product.actions.ts
+++ src/ui/actions/product.actions.ts
@@ -1,18 +1,18 @@
-// src/ui/actions/product.actions.ts
-'use server';
+// // src/ui/actions/product.actions.ts
+// 'use server';
 
-import { CreateProductUseCase } from "@/domain/use-cases/create-product";
-import { CreateProductInput } from "@/domain/entities/product.entity";
+// import { CreateProductUseCase } from "@/domain/use-cases/create-product";
+// import { CreateProductInput } from "@/domain/entities/product.entity";
 
-export async function createProductAction(data: CreateProductInput) {
-    try {
-        const useCase = new CreateProductUseCase();
-        const product = useCase.execute(data);
+// export async function createProductAction(data: CreateProductInput) {
+//     try {
+//         const useCase = new CreateProductUseCase();
+//         const product = useCase.execute(data);
 
-        // Aquí más adelante llamaremos a la capa de persistencia (ej. DB)
+//         // Aquí más adelante llamaremos a la capa de persistencia (ej. DB)
 
-        return { success: true, data: product };
-    } catch (error: any) {
-        return { success: false, error: error.message };
-    }
-}
+//         return { success: true, data: product };
+//     } catch (error: any) {
+//         return { success: false, error: error.message };
+//     }
+// }
